4bb0e0d8427c7fbde2b5638a40a6216872e9a55a
shearappold <shearappold@gmail.com>
2022-10-09 14:46:04 -0400

    Commit Stubs

---
+++ Command/Document.java
@@ -0,0 +1,28 @@
+package Command;
+
+import java.util.ArrayList;
+
+public class Document {
+    private String fileName;
+    private ArrayList<String> lines= new ArrayList<>();
+
+    public Document(String fileName){
+        this.fileName=fileName;
+    }
+
+    public String view(){
+        return "";
+    }
+
+    public String append(String line){
+        return "";
+    }
+
+    public String write(String line){
+        return "";
+    }
+
+    public String save(){
+        return "";
+    }
+}
---
+++ Command/InputHandler.java
@@ -0,0 +1,17 @@
+package Command;
+
+import javax.swing.text.Document;
+
+public class InputHandler {
+    private String HashMap(String command){
+        return "";
+    }
+
+    public Document InputHandler(Document document){
+        return document;
+    }
+
+    public void inputEntered(String data){
+
+    }
+}
