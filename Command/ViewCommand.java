2095d9471a54568de6194611bfcfc67925b2cc11
shearappold <shearappold@gmail.com>
2022-10-09 20:21:58 -0400

    Command Stubs

---
+++ Command/ViewCommand.java
@@ -0,0 +1,11 @@
+package Command;
+
+public class ViewCommand extends Command{
+    public ViewCommand(Document doc){
+        super(doc);
+    }
+
+    public String execute(){
+        return null;
+    }
+}
